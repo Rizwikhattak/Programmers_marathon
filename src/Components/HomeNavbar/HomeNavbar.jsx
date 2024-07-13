effffd084d143da97129768a7e4aabc580813ee1
Rizwikhattak <rizwikhattak4384@gmail.com>
2024-07-13 18:24:58 +0500

    Starting with website

--- src/Components/HomeNavbar/HomeNavbar.jsx
+++ src/Components/HomeNavbar/HomeNavbar.jsx
@@ -1,35 +1,12 @@
 import React from "react";
-import logo from "../../Assets/images/logo.png";
-import { IoSearch } from "react-icons/io5";
-const HomeNavbar = () => {
+
+function HomeNavbar() {
   return (
-    <nav className="bg-[#245C51] p-5 flex flex-row justify-between align-middle">
-      <div className="logo-side p-5 flex flex-row justify-between align-middle">
-        <div className="logo">
-          <img src={logo} alt="Logo" />
-        </div>
-        {/* <div className="categories px-5">
-          <button></button>
-        </div> */}
-      </div>
-      <div className="pages p-5 text-white">
-        <ul className="flex justify-between align-middle gap-10">
-          <li>Home</li>
-          <li>Pages</li>
-          <li>Instructor</li>
-          <li>Course</li>
-          <li>Blog</li>
-        </ul>
-      </div>
-      <div className="search-login">
-        <input
-          type="text"
-          placeholder={`${(<IoSearch />)}Search Courses`}
-          className="p-5 rounded-full"
-        />
-      </div>
+    <nav className="navbad">
+      <div className="navbar-logo"></div>
+      <div className="main-pages"></div>
     </nav>
   );
-};
+}
 
 export default HomeNavbar;
