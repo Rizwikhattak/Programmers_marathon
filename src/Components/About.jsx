7e98ae6d1b1c80a779357affe8a62bacc82b13d9
Rizwikhattak <rizwikhattak4384@gmail.com>
2024-07-15 20:30:10 +0800

    working with about images

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CircleAboutImg from "../Assets/circleAboutImg.png";
 import MainAboutImg from "../Assets/MainAboutImg.png";
+import headphones from "../Assets/headphones.png";
 import { IoMdCheckboxOutline } from "react-icons/io";
+import CountUp from "react-countup";
 const About = () => {
+  const [state1, setState1] = useState();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop >= 800) {
+        setState1(<CountUp end={56} duration={3} />);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <section className="About-Section text-white bg-[#242424] h-full w-full flex px-12 py-24 gap-x-10">
-      <div className="about-images w-[45%]">
-        <div className="about-img-1">
+      <div className="about-images w-[50%] relative">
+        <div className="about-img-1 pl-5">
           <img src={MainAboutImg} alt="" />
         </div>
-        <div className="about-img-2">
-          <img src="" alt="" />
+        <div className="about-img-2 bg-white inline-block p-3 absolute top-72 left-0">
+          <div className="black-border border-[15px] border-black py-5 pr-5">
+            <div className="count-up-content flex gap-x-10">
+              <div
+                className="stick text-white bg-[#FFAB17]"
+                style={{
+                  clipPath:
+                    "polygon(0% 0%, 75% 0%, 100% 50%,75% 100%, 0% 100%)",
+                }}
+              >
+                <h1 className="flex  items-center pl-4 pr-5 py-2 relative mr-4 text-2xl font-bold">
+                  {state1} +
+                </h1>
+              </div>
+
+              <img src={headphones} alt="" />
+            </div>
+            <div className="pt-5">
+              <h1 className="text-[#FFAB17] text-xl font-bold text-center">
+                Experience
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="about-content relative w-[55%] pr-10">
+      <div className="about-content relative w-[50%] pr-10">
         <div className="content-img absolute right-0 top-0">
           <img src={CircleAboutImg} alt="" />
         </div>
