5312beb901eeb9546eb5a79b2942a5bd9b4d30dc
Rizwikhattak <rizwikhattak4384@gmail.com>
2024-07-19 22:34:12 +0500

    carousel..

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./ProductsPage.css";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 import ProductImg_1 from "../Assets/ProductImg_1.jpeg";
@@ -6,61 +6,29 @@ import ProductImg_2 from "../Assets/ProductImg_2.jpeg";
 import ProductImg_3 from "../Assets/ProductImg_3.jpeg";
 import Slider from "react-slick";
 
-const CustomNextArrow = (props) => {
-  const { className, onClick } = props;
-  return (
-    <div className="custom-arrow-container custom-next-container">
-      <div
-        className={`${className} custom-arrow custom-next`}
-        onClick={onClick}
-      />
-    </div>
-  );
-};
-
-const CustomPrevArrow = (props) => {
-  const { className, onClick } = props;
-  return (
-    <div className="custom-arrow-container custom-prev-container">
-      <div
-        className={`${className} custom-arrow custom-prev`}
-        onClick={onClick}
-      />
-    </div>
-  );
-};
-
 const Products = () => {
   const [checkCurrent, setCheckCurrent] = useState(1);
+  const sliderRef = useRef(null);
 
   const handleClick = (index) => {
     setCheckCurrent(index);
   };
 
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    nextArrow: <CustomNextArrow />,
-    prevArrow: <CustomPrevArrow />,
-    responsive: [
-      {
-        breakpoint: 960,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+    arrows: false,
+  };
+
+  const handlePrev = () => {
+    sliderRef.current.slickPrev();
+  };
+
+  const handleNext = () => {
+    sliderRef.current.slickNext();
   };
 
   return (
@@ -93,28 +61,51 @@ const Products = () => {
           )}
         </ul>
       </div>
-      <div className="products-carousel">
-        <div className="product-images-section h-full w-full p-10 flex justify-between items-center gap-5">
-          {productData.map((currElem, currIndex) => {
-            return (
-              <div className={`product-${currIndex + 1} relative`}>
-                <div className="">
-                  <img src={currElem.img} alt="" className="" />
+      <div className="products-carousel p-5">
+        <Slider ref={sliderRef} {...settings}>
+          {productData.map((currElem, currIndex) => (
+            <div
+              className={`product-${currIndex + 1} relative`}
+              key={currIndex}
+            >
+              <div>
+                <img src={currElem.img} alt="" className="" />
+              </div>
+              <div className="products-content flex justify-between bg-white text-black p-5  rounded-xl">
+                <div className="prod-content">
+                  <h1 className="font-bold text-2xl">{currElem.title}</h1>
+                  <p className="text-sm">{currElem.description}</p>
+                </div>
+                <div className="prod-icon">
+                  <span className="group right-carousel-btn p-3 bg-white rounded-full flex justify-center items-center hover:bg-[#FFAB17] transition duration-300 ease-in-out cursor-pointer">
+                    <button
+                      className="right-btn p-1 bg-[#FFAB17] rounded-full text-white group-hover:bg-white group-hover:text-[#FFAB17] transition duration-300 ease-in-out"
+                      onClick={handleNext}
+                    >
+                      <FaAngleRight />
+                    </button>
+                  </span>
                 </div>
               </div>
-            );
-          })}
-        </div>
+            </div>
+          ))}
+        </Slider>
         <div className="flex justify-end items-center pr-40">
           <div className="carousel-buttons flex items-center py-5">
-            <span className=" group left-carousel-btn p-3 mr-5 bg-white rounded-full flex justify-center items-center hover:bg-[#FFAB17] transition duration-300 ease-in-out">
-              <button className="left-btn p-1 bg-[#FFAB17] rounded-full text-white">
+            <span className="group left-carousel-btn p-3 mr-5 bg-white rounded-full flex justify-center items-center hover:bg-[#FFAB17] transition duration-300 ease-in-out cursor-pointer">
+              <button
+                className="left-btn p-1 bg-[#FFAB17] rounded-full text-white group-hover:bg-white group-hover:text-[#FFAB17] transition duration-300 ease-in-out"
+                onClick={handlePrev}
+              >
                 <FaAngleLeft />
               </button>
             </span>
 
-            <span className="left-carousel-btn p-3 bg-white rounded-full flex justify-center items-center">
-              <button className="left-btn p-1 bg-[#FFAB17] rounded-full text-white">
+            <span className="group right-carousel-btn p-3 bg-white rounded-full flex justify-center items-center hover:bg-[#FFAB17] transition duration-300 ease-in-out cursor-pointer">
+              <button
+                className="right-btn p-1 bg-[#FFAB17] rounded-full text-white group-hover:bg-white group-hover:text-[#FFAB17] transition duration-300 ease-in-out"
+                onClick={handleNext}
+              >
                 <FaAngleRight />
               </button>
             </span>
